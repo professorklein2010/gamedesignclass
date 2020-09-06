b77a113f330893dbb0b602fe993fe1e8b03135dd
professorklein2010 <35984481+professorklein2010@users.noreply.github.com>
2020-09-06 14:54:42 -0400

    Add files via upload

---
+++ EGPSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EGPSpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int startingWave = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnAllEGPInWave(currentWave));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   private IEnumerator SpawnAllEGPInWave (WaveConfig wavConfig)
+   {
+       for (int EGPCount = 0; EGPCount < wavConfig.GetNumberOfEnemies(); EGPCount ++)
+       {
+       Instantiate(
+       wavConfig.GetEnemyPrefab(), 
+       wavConfig.GetWaypoints()[0].transform.position, 
+       Quaternion.identity);
+       yield return new WaitForSeconds(wavConfig.GetTimeBetweenSpawns());
+       }
+
+       
+
+   }
+}
